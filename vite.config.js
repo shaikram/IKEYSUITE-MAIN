d71878ec3b237291de9c6ab7570a5f02d9a09154
Shaikram M. Abdulguro Jr. <SLEC67-733@STLUKES67.CLINIC>
2026-09-17 11:50:58 +0800

    Fix port on cloud

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT) || 80, // Reads $PORT on Linux/Cloud, falls back to 80 locally
+  },
 })
