131fdc0b61b514c375e9f14c4fa11cd2a5b7044a
Shaikram M. Abdulguro Jr. <SLEC67-733@STLUKES67.CLINIC>
2026-09-17 13:54:53 +0800

    Fix 2

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   preview: {
     host: '0.0.0.0',
-    port: parseInt(process.env.PORT) || 80, // Reads $PORT on Linux/Cloud, falls back to 80 locally
+    port: process.env.PORT ? parseInt(process.env.PORT) : 4173, // Reads $PORT on Linux/Cloud, falls back to 80 locally
     strictPort: true, // Forces Vite to fail if port 80/PORT is unavailable instead of changing to 3000
     allowedHosts: true // Allows proxy requests from Nginx/hosting domain
   },
